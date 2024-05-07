32c17e47370fa44669be9e7e9e77ea21d96571f1
jlarios <jared@totaltransportlogistics.us>
2024-05-07 15:19:58 -0600

    fix: script machin called

--- program/scripts/InstallWinget.ps1
+++ program/scripts/InstallWinget.ps1
@@ -3,5 +3,5 @@ Param (
     [string]$PName
 )
 
-winget.exe install --machine -e --id $PName --accept-source-agreements
+winget.exe install -e --id $PName --accept-source-agreements
 Write-Host "[+] $PName installed"
